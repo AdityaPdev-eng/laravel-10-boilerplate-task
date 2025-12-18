217fc395226711fea529f3697b0d46e0ac8f856a
AdityaPdev-eng <adityapdev525@gmail.com>
2025-12-18 10:54:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,4 +28,9 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
+provider "aws" {
+  region = var.aws_region
+}
+
+
 
